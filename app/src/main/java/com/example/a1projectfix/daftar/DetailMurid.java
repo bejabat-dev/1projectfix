66acb5635fd0b8f33f76698b635c7eff375ebb6a
bejabat-dev <bejabattailor@gmail.com>
2024-08-12 16:48:41 +0700

    1.0

--- app/src/main/java/com/example/a1projectfix/daftar/DetailMurid.java
+++ app/src/main/java/com/example/a1projectfix/daftar/DetailMurid.java
@@ -1,4 +1,4 @@
-package com.example.a1projectfix;
+package com.example.a1projectfix.daftar;
 
 import android.os.Bundle;
 
@@ -8,13 +8,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class DataMurid extends AppCompatActivity {
+import com.example.a1projectfix.R;
+
+public class DetailMurid extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_data_murid);
+        setContentView(R.layout.activity_detail_murid);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/example/a1projectfix/utilitas/DataMurid.java
+++ app/src/main/java/com/example/a1projectfix/utilitas/DataMurid.java
@@ -24,6 +24,7 @@ import java.util.HashMap;
 public class DataMurid {
     public DataMurid() {
     }
+
     private static String nama;
     private static String kelas;
     public static String key;
@@ -31,19 +32,15 @@ public class DataMurid {
     public static String getNama() {
         return nama;
     }
-
     public static String getKelas() {
         return kelas;
     }
-
     public static String getTempat() {
         return tempat;
     }
-
     public static String getTanggal() {
         return tanggal;
     }
-
     public static String getAlamat() {
         return alamat;
     }
@@ -52,8 +49,8 @@ public class DataMurid {
     private static String tanggal;
     private static String alamat;
 
-    public static String foto1,foto2,foto3;
-    private static ArrayList<HashMap<String,Object>> list_murid;
+    public static String foto1, foto2, foto3;
+    private static ArrayList<HashMap<String, Object>> list_murid;
 
     public static ArrayList<HashMap<String, Object>> getList_murid() {
         return list_murid;
@@ -63,10 +60,10 @@ public class DataMurid {
         return data_murid;
     }
 
-    public static HashMap<String,Object> data_murid;
-    public static HashMap<String,Object> edit;
+    public static HashMap<String, Object> data_murid;
+    public static HashMap<String, Object> edit;
 
-    public DataMurid(String key, String nama, String kelas, String tempat, String tanggal, String alamat){
+    public DataMurid(String key, String nama, String kelas, String tempat, String tanggal, String alamat) {
 
         DataMurid.nama = nama;
         DataMurid.kelas = kelas;
@@ -74,43 +71,43 @@ public class DataMurid {
         DataMurid.tanggal = tanggal;
         DataMurid.alamat = alamat;
         data_murid = new HashMap<>();
-        data_murid.put("key",key);
-        data_murid.put("nama",nama);
-        data_murid.put("kelas",kelas);
-        data_murid.put("tempat",tempat);
-        data_murid.put("tanggal",tanggal);
-        data_murid.put("alamat",alamat);
-
-        data_murid.put("foto1","");
-        data_murid.put("foto2","");
-        data_murid.put("foto3","");
+        data_murid.put("key", key);
+        data_murid.put("nama", nama);
+        data_murid.put("kelas", kelas);
+        data_murid.put("tempat", tempat);
+        data_murid.put("tanggal", tanggal);
+        data_murid.put("alamat", alamat);
+        data_murid.put("foto1", "");
+        data_murid.put("foto2", "");
+        data_murid.put("foto3", "");
     }
 
-    public void tambahMurid(Context c, HashMap<String,Object> map, String key){
+    public void tambahMurid(Context c, HashMap<String, Object> map, String key) {
         DatabaseReference db = FirebaseDatabase.getInstance().getReference();
         db.child("Murid").child(key).setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
-                if(task.isSuccessful()){
-                    Toast.makeText(c,"Murid berhasil ditambahkan",Toast.LENGTH_SHORT).show();
+                if (task.isSuccessful()) {
+                    Toast.makeText(c, "Murid berhasil ditambahkan", Toast.LENGTH_SHORT).show();
                     c.startActivity(new Intent(c, TambahFoto.class));
-                    ((Activity)c).finish();
-                }else{
-                    Toast.makeText(c,"Terjadi kesalahan",Toast.LENGTH_SHORT).show();
+                    ((Activity) c).finish();
+                } else {
+                    Toast.makeText(c, "Terjadi kesalahan", Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
 
-    public static void loadMurid(){
+    public static void loadMurid() {
         list_murid = new ArrayList<>();
         DatabaseReference db = FirebaseDatabase.getInstance().getReference();
         db.child("Murid").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                GenericTypeIndicator<HashMap<String,Object>> map = new GenericTypeIndicator<HashMap<String, Object>>() {};
-                for(DataSnapshot ds : snapshot.getChildren()){
-                    HashMap<String,Object> d = ds.getValue(map);
+                GenericTypeIndicator<HashMap<String, Object>> map = new GenericTypeIndicator<HashMap<String, Object>>() {
+                };
+                for (DataSnapshot ds : snapshot.getChildren()) {
+                    HashMap<String, Object> d = ds.getValue(map);
                     list_murid.add(d);
                 }
             }
